30218a1125019ed5d0eb433a48922aec6072e42b
smirnovsky <happy280190@gmail.com>
2022-12-28 15:19:04 +0300

    feat: add functionality and styles for empty cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,23 @@ import { FaShoppingCart } from 'react-icons/fa';
 import Order from './Order';
 
 
+const showOrders = (props) => {
+  return (
+    <div>
+      {props.orders.map(el => (
+        <Order key={el.id} item={el} />
+      ))}
+    </div>
+  )
+}
 
+const showNothing = () => {
+  return (
+    <div className='empty'>
+      <h2>Empty</h2>
+    </div>
+  )
+}
 
 export default function Header(props) {
   let [cartOpen, setCartOpen] = useState(false)
@@ -21,9 +37,8 @@ export default function Header(props) {
 
             {cartOpen && (
               <div className='shop-cart'>
-                {props.orders.map(el => (
-                  <Order key={el.id} item={el} />
-                ))}
+                {props.orders.length > 0 ?
+                  showOrders(props) : showNothing()}
               </div>
             )}
         </div>
